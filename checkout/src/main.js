a7ed20fe2156c65735414f52ec45cb525132b0f1
Jakub Kida <jakub@snapp.dk>
2022-07-04 00:24:05 +0200

    Forms init

--- checkout/src/main.js
+++ checkout/src/main.js
@@ -1,11 +1,14 @@
-import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createApp } from "vue";
+import "./assets/base.css";
+import { plugin, defaultConfig } from "@formkit/vue";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+app.use(plugin, defaultConfig);
 app.use(createPinia());
 app.use(router);
 
--- checkout/src/views/HomeView.vue
+++ checkout/src/views/HomeView.vue
@@ -1,9 +1,48 @@
-<script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+<script>
+async function save(credentials) {
+  console.log(credentials);
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="px-6">
+      <FormKit
+        type="form"
+        id="registration-example"
+        submit-label="Sign In"
+        @submit="save"
+        :actions="false"
+        :value="{
+          firstName: 'Andrzej',
+          lastName: 'Malka',
+          city: 'Polkowice',
+        }"
+      >
+        <FormKit
+          input-class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          outer-class="pt-12"
+          label-class="block text-yellow-700 text-sm font-bold mb-2"
+          validation="required"
+          label="First name"
+          name="firstName"
+        />
+        <FormKit
+          input-class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          outer-class="pt-4"
+          label-class="block text-yellow-700 text-sm font-bold mb-2"
+          validation="required"
+          label="Last name"
+          name="lastName"
+        />
+        <FormKit
+          input-class="bg-white-700 font-bold mt-2 py-2 rounded"
+          type="submit"
+          label="Register"
+        />
+      </FormKit>
+    </div>
   </main>
 </template>
